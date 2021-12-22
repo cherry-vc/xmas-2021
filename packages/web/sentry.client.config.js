7dccc721f766779aa8eef836bb792a1553ee8e42
Brett Sun <qisheng.brett.sun@gmail.com>
2021-12-22 22:41:50 +0000

    chore: update sentry dsn

--- packages/web/sentry.client.config.js
+++ packages/web/sentry.client.config.js
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN =
   process.env.SENTRY_DSN ||
   process.env.NEXT_PUBLIC_SENTRY_DSN ||
-  'https://cca39ced93b04586b7b3426376280745@o1097219.ingest.sentry.io/6118513'
+  'https://2f93bb852b574a2f8a9169dfdebbb5e3@o1097219.ingest.sentry.io/6118716'
 
 Sentry.init({
   dsn: process.env.NODE_ENV === 'production' ? SENTRY_DSN : null,
