a962dea118719c9eecd41465f10a6ca191778a68
Brett Sun <qisheng.brett.sun@gmail.com>
2021-12-22 16:46:02 +0000

    web: add api stubs

---
+++ packages/web/pages/api/fragment/[addr].js
@@ -0,0 +1,22 @@
+import { ethers } from 'ethers'
+
+import environment, { setupEthers } from '../../environment/api'
+
+const { provider } = setupEthers()
+
+export default function handler(req, res) {
+  const { method } = req
+
+  if (method !== 'GET') {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).end(`Method ${method} Not Allowed`)
+    return
+  }
+
+  const { addr } = req.query
+
+  // TODO:
+  //   1. instantiate + connect contract
+  //   2. return nft.tokensOf(addr) (and sort it here)
+  res.status(200).json({ tokens: [] })
+}
