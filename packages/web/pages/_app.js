2894c16bce834d81ecd3e9af597b12579eb62040
Brett Sun <qisheng.brett.sun@gmail.com>
2021-12-24 04:20:54 +0100

    web: fix head's title warning by pushing it to _app instead (#17)

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import { AppProvider } from '../context/AppContext'
 import Layout from '../components/Layout'
 
 function App({ Component, pageProps }) {
   return (
     <AppProvider>
+      <Head>
+        <title>Cherry - Xmas 2021</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
