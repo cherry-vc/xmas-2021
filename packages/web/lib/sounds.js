178c043c95d1961658e703ab8e1978f23e3d1ac8
Brett Sun <qisheng.brett.sun@gmail.com>
2021-12-24 03:08:50 +0000

    web: add soundtrack to claiming

---
+++ packages/web/lib/sounds.js
@@ -0,0 +1,8 @@
+const sounds = {
+  urls: {
+    trimmed: 'https://ipfs.io/ipfs/QmbiTa8jtADff8ce8Lu1ksadMoKuZhHxBPV4fJHhXJu4mu/soundtrack.mp3',
+    full: 'https://ipfs.io/ipfs/QmbiTa8jtADff8ce8Lu1ksadMoKuZhHxBPV4fJHhXJu4mu/soundtrack_full.mp3',
+  },
+}
+
+export default sounds
