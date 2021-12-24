35dc6ec489961b7776e17d4739fab8811b19beea
Brett Sun <qisheng.brett.sun@gmail.com>
2021-12-24 01:57:32 +0000

    web: fix sentry webpack config failing when sentry dsn is not available (e.g. in dev and preview environments)

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -4,6 +4,8 @@
 //     - https://docs.sentry.io/platforms/javascript/guides/nextjs/
 const { withSentryConfig } = require('@sentry/nextjs')
 
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig({}, { silent: true })
+module.exports = !!SENTRY_DSN ? withSentryConfig({}, { silent: true }) : {}
